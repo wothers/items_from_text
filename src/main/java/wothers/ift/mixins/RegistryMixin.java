dbb2aee32dda1b8f3a68c0b571fd737018ec46b9
wothers <reggspoons@gmail.com>
2023-06-24 20:00:51 +0100

    Added mixin for internally validating identifier type property values

---
+++ src/main/java/wothers/ift/mixins/RegistryMixin.java
@@ -0,0 +1,56 @@
+package wothers.ift.mixins;
+
+import com.mojang.datafixers.util.Pair;
+import com.mojang.logging.LogUtils;
+import com.mojang.serialization.Lifecycle;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.util.registry.SimpleRegistry;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+@Mixin(SimpleRegistry.class)
+public abstract class RegistryMixin {
+    private final Map<RegistryKey<?>, FoodComponent> foodComponentMap = new HashMap<>();
+
+    @Inject(method = "add", at = @At(value = "HEAD"))
+    private void interceptAddItem(RegistryKey<?> key, Object entry, Lifecycle lifecycle, CallbackInfoReturnable<RegistryEntry<?>> cir) {
+        if (!this.equals(Registry.ITEM)) return;
+        addFoodContainingEffects(key, (Item) entry);
+    }
+
+    private void addFoodContainingEffects(RegistryKey<?> key, Item item) {
+        if (!item.isFood()) return;
+        FoodComponent foodComponent = item.getFoodComponent();
+        if (foodComponent.getStatusEffects().isEmpty()) return;
+        foodComponentMap.put(key, foodComponent);
+    }
+
+    @Inject(method = "freeze", at = @At(value = "HEAD"))
+    private void validateOnFreeze(CallbackInfoReturnable<Registry<?>> cir) {
+        if (!this.equals(Registry.ITEM)) return;
+        validateFoodEffects();
+    }
+
+    private void validateFoodEffects() {
+        for (Map.Entry<RegistryKey<?>, FoodComponent> entry : foodComponentMap.entrySet()) {
+            Iterator<Pair<StatusEffectInstance, Float>> iterator = entry.getValue().getStatusEffects().iterator();
+            while (iterator.hasNext()) {
+                Pair<StatusEffectInstance, Float> pair = iterator.next();
+                if (pair.getFirst().getEffectType() == null) {
+                    iterator.remove();
+                    LogUtils.getLogger().error("Food item {} has invalid status effect(s)", entry.getKey().getValue());
+                }
+            }
+        }
+    }
+}
