7eebbdd057be715a7057634f88264275c9f583d4
Erik Steiner <estebanium@mailbox.org>
2024-01-03 00:05:34 +0100

    Update to 1.19.2

--- src/main/java/wothers/ift/mixins/RegistryMixin.java
+++ src/main/java/wothers/ift/mixins/RegistryMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.util.registry.SimpleRegistry;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -20,29 +21,33 @@ import java.util.Map;
 
 @Mixin(SimpleRegistry.class)
 public abstract class RegistryMixin {
-    private final Map<RegistryKey<?>, FoodComponent> foodComponentMap = new HashMap<>();
+
+    @Unique
+    private final Map<RegistryKey<?>, FoodComponent> itemsfromtext$foodComponentMap = new HashMap<>();
 
     @Inject(method = "add", at = @At(value = "HEAD"))
     private void interceptAddItem(RegistryKey<?> key, Object entry, Lifecycle lifecycle, CallbackInfoReturnable<RegistryEntry<?>> cir) {
         if (!this.equals(Registry.ITEM)) return;
-        addFoodContainingEffects(key, (Item) entry);
+        itemsfromtext$addFoodContainingEffects(key, (Item) entry);
     }
 
-    private void addFoodContainingEffects(RegistryKey<?> key, Item item) {
+    @Unique
+    private void itemsfromtext$addFoodContainingEffects(RegistryKey<?> key, Item item) {
         if (!item.isFood()) return;
         FoodComponent foodComponent = item.getFoodComponent();
         if (foodComponent.getStatusEffects().isEmpty()) return;
-        foodComponentMap.put(key, foodComponent);
+        itemsfromtext$foodComponentMap.put(key, foodComponent);
     }
 
     @Inject(method = "freeze", at = @At(value = "HEAD"))
     private void validateOnFreeze(CallbackInfoReturnable<Registry<?>> cir) {
         if (!this.equals(Registry.ITEM)) return;
-        validateFoodEffects();
+        itemsfromtext$validateFoodEffects();
     }
 
-    private void validateFoodEffects() {
-        for (Map.Entry<RegistryKey<?>, FoodComponent> entry : foodComponentMap.entrySet()) {
+    @Unique
+    private void itemsfromtext$validateFoodEffects() {
+        for (Map.Entry<RegistryKey<?>, FoodComponent> entry : itemsfromtext$foodComponentMap.entrySet()) {
             Iterator<Pair<StatusEffectInstance, Float>> iterator = entry.getValue().getStatusEffects().iterator();
             while (iterator.hasNext()) {
                 Pair<StatusEffectInstance, Float> pair = iterator.next();
